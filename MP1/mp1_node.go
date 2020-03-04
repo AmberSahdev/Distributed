2d83a201b9ffa463125897205784b6d9ec82519b
navidm2 <navidm2@illinois.edu>
2020-03-04 01:35:59 -0600

    Outlined Incoming Data reception, but not handling.

--- MP1/mp1_node.go
+++ MP1/mp1_node.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"encoding/gob"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"net"
 	"os"
@@ -17,6 +16,7 @@ var numNodes int // specified parameter, number of starting nodes
 var numConns int // tracks number of other nodes connected to this node
 var nodeNum int  // tracks local node's number
 var nodeList []nodeComms
+var localReceivingMessages chan message
 
 type nodeComms struct {
 	number      int
@@ -70,13 +70,14 @@ func rMulticast(m message) {
 }
 
 func (destNode *nodeComms) communicationTask() {
-	var err error = nil
-	var m message
-	enc := gob.NewEncoder(destNode.conn)
-	defer destNode.setConnected(false)
-	for err != nil {
-		m <- destNode.outbox
-		err = enc.Encode(m)
+	tcpEnc := gob.NewEncoder(destNode.conn)
+	defer destNode.conn.Close()
+	for m := range destNode.outbox {
+		err := tcpEnc.Encode(m)
+		if err != nil {
+			fmt.Println(os.Stderr, "Failed to send message, receiver down?")
+			return
+		}
 	}
 }
 
@@ -87,78 +88,48 @@ func (destNode *nodeComms) openOutgoingConn() {
 	go destNode.communicationTask()
 }
 
+// ran when the incoming connection to this node throws and errror
 func (destNode *nodeComms) closeOutgoingConn() {
+	destNode.isConnected = false
 	numConns--
-	destNode.conn.Close()
 	close(destNode.outbox)
 }
 
 func parseHostTextfile(path string) []string {
 	dat, err := ioutil.ReadFile(path)
 	check(err)
-	return strings.Split(dat, "\n")
+	return strings.Split(string(dat), "\n")
 }
 
-func handleIncomingConn(conn net.Conn) {
-	buf := make([]byte, 1048576) // Make 1MB buffer to hold incoming data
-	incomingNodeNum := -1
+func receiveIncomingData(conn net.Conn) {
+	incomingNodeNum := -1 // no known node number yet
+	var m message
+	tcpDecode := gob.NewDecoder(conn)
 	for {
-		inputLen, err := conn.Read(buf)
-		now := time.Now()
-		nanoseconds := float64(now.UnixNano()) / 1e9
+		err := tcpDecode.Decode(&m)
 		if err != nil {
+			now := time.Now()
+			nanoseconds := float64(now.UnixNano()) / 1e9
 			fmt.Printf("%f - Node %d disconnected\n", nanoseconds, incomingNodeNum)
 			return
 		}
-		eventList := strings.Split(string(buf[:inputLen]), "\n")
-		for i := 0; i < len(eventList); i++ {
-			if len(eventList[i]) == 0 {
-				if i != len(eventList)-1 {
-					panic("Why is this an empty transaction?")
-				}
-				continue
-			}
-			now = time.Now()
-			nanoseconds = float64(now.UnixNano()) / 1e9
-			if nodeName == "" {
-				nodeName = eventList[i]
-			}
-			contents := strings.Split(eventList[i], " ")
-			contentLen := len(contents)
-			transmittedTime := 0.0
-			fmt.Printf("%f ", nanoseconds)
-			if contentLen == 2 {
-				transmittedTime, err = strconv.ParseFloat(contents[0], 64)
-				check(err)
-				fmt.Println(nodeName + " " + contents[1])
-			} else if contentLen == 1 {
-				fmt.Println("- " + nodeName + " connected")
-			} else {
-				fmt.Println("\n%d", contentLen)
-				panic("Why not be length 1 or 2")
-			}
-
-			// Writing to files delay.txt and bandwidth.txt
-			// filePointers[0].WriteString(fmt.Sprintf("%f ", nanoseconds-transmittedTime))
-			// filePointers[1].WriteString(fmt.Sprintf("%d ", inputLen))
-		}
+		localReceivingMessages <- m
 	}
 }
 
-func handleIncomingConns(port string) {
-	listener, err := net.Listen("tcp", ":"+port)
-	check(err)
+func handleAllIncomingConns(listener net.Listener) {
 	defer listener.Close()
 	for {
 		conn, err := listener.Accept()
 		check(err)
-		go handleIncomingConn(conn) // open up a goroutine
+		go receiveIncomingData(conn) // open up a go routine
 	}
 }
 
-func startHandlingIncomingConns(port string) {
-	go handleIncomingConns(port)
-	return
+func openListener(port string) net.Listener {
+	listener, err := net.Listen("tcp", ":"+port) // open port
+	check(err)
+	return listener
 }
 
 // handles stdin transaction messaging
@@ -173,7 +144,8 @@ func handleLocalEventGenerator() {
 
 func setupConnections(port string, hostList []string) {
 	var err error
-	startHandlingIncomingConns(port)
+	listener := openListener(port)
+	go handleAllIncomingConns(listener)
 	for curNodeNum := 0; curNodeNum < numNodes; curNodeNum++ {
 		nodeList[curNodeNum].port = port
 		nodeList[curNodeNum].address = hostList[curNodeNum]
