475c0c705fc86522d76af0467929bab141743984
navidm2 <navidm2@illinois.edu>
2020-03-05 02:49:51 -0600

    fixed redelivery issue.

--- MP1/mp1_node.go
+++ MP1/mp1_node.go
@@ -167,7 +167,7 @@ func (m message) isProposal() bool {
 }
 
 func (m *message) setTransactionID() {
-	m.transactionId = (uint64(localNodeNum) << (64 - 8)) | (m.senderMessageNumber & 0x00FFFFFFFFFFFFFF) // {originalSender, senderMessageNumber[55:0]}
+	m.transactionId = (uint64(localNodeNum) << (64 - 8)) | (uint64(m.senderMessageNumber) & 0x00FFFFFFFFFFFFFF) // {originalSender, senderMessageNumber[55:0]}
 }
 
 // TODO Biggest Fuck, drains the message Channel
@@ -203,6 +203,7 @@ func handleMessageChannel() {
 			}
 		}
 		// delivery of message to ISIS handler occurs here
+		// Receiving message 2 and sending message 3 handled here
 		if m.isProposal() {
 			idx := pq.find(m.transactionId)
 
@@ -230,9 +231,10 @@ func handleMessageChannel() {
 				_ = heap.Pop(pq).(*Item) // TODO: put it into our account balances
 				m = pq[0].value
 			}
-
-		} else if m.needsProposal() { // external message needing proposal
+		} else if m.needsProposal() { // TODO Receiving message 1 and sending message 2 handled here
 			m.proposeSequenceNum()
+		} else if m.isFinal { // TODO Receiving message 3 here
+
 		}
 	}
 }
