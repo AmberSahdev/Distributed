e7007468cbf228a9ec28865c5612e57777e9e69f
Amber Sahdev <asahdev2@illinois.edu>
2020-05-05 19:49:42 -0500

    working on client

--- MP3/src/client/main.go
+++ MP3/src/client/main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+var branches map[string]net.Conn
 
 func main() {
 	fmt.Println("I'm a Client")
+	branches = make(map[string]net.Conn)
+
+	connectToBranches()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		input := scanner.Text()
+
+		if input == "BEGIN" {
+			for scanner.Scan() {
+				input = scanner.Text()
+
+				if input == "ABORT" {
+					// TODO
+				} else {
+					targetBranchName := findInputTarget(input)
+					_, err := branches[targetBranchName].Write([]byte(input))
+					check(err)
+
+					for {
+						// if received an OK, exit loop. (wait for lock + tcp reply)
+						// else see if user typed in "Abort"
+					}
+				}
+
+			}
+		} else {
+			//Discard Input
+		}
+	}
+
+	/*
+		1. accept user input of BEGIN, DEPOSIT, BALANCE, WITHDRAW, COMMIT, ABORT
+		2. wait for relevant branch to reply "OK", a balance, "NOT FOUND", "ABORTED", "COMMIT OK"
+			- if branch replies
+		3. repeat
+	*/
 }
--- MP3/src/client/utils.go
+++ MP3/src/client/utils.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		fmt.Println("Error Detected:")
+		panic(e)
+	}
+}
+
+func connectToBranches() {
+	file, err := os.Open("../branchAddresses.txt")
+	check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		nameAddressPort := strings.Split(scanner.Text(), ":")
+		branches[nameAddressPort[0]], err = net.Dial("tcp", nameAddressPort[1]+":"+nameAddressPort[2])
+		check(err)
+	}
+	fmt.Println("Connected to all branches in branchAddresses.txt")
+}
+
+func findInputTarget(input string) string {
+	// TODO: parse target branch A, or B, or C ... from input
+	return ""
+}
